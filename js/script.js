2a2baa13d2a8bd0594099259c72ab96879e4e6a8
yehwansong <yehwanyensong@gmail.com>
2019-09-04 12:47:01 +0900

    add

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+jQuery( document ).ready(function() {
+	// var w = window.innerWidth
+	// var h = window.innerHeight
+	// if(w>h >  123/90){
+	// 	jQuery('.schedule_wrap').css({'width':jQuery('.text_whole_sub_wrap').outerWidth(true)})
+	// }else{
+	// 	jQuery('.schedule_wrap').css({'width':'100vw'})
+	// }
+
+});
